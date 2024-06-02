c1a2deae5f7ac45d801cc3876c150f54faea5590
Ioana Botezatu <115404201+IoanaBotezatu01@users.noreply.github.com>
2024-06-02 18:13:30 +0300

    Add files via upload

---
+++ Lab2-baze-Win/Lab1-baze-Win/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Lab1_baze_Win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Shops";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(590, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tables";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 325);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AccessibleName = "Owners";
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(441, 53);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.Size = new System.Drawing.Size(347, 231);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AccessibleName = "Shops";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 53);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(320, 231);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private DataGridView dataGridView2;
+        private DataGridView dataGridView1;
+    }
+}
+
---
+++ Lab2-baze-Win/Lab1-baze-Win/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Lab1_baze_Win
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter daShops;
+        SqlDataAdapter daCoffeeTables;
+        DataSet ds;
+        BindingSource bsShops;
+        BindingSource bsCoffeeTables;
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.dataGridView1.SelectionMode= DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView2.SelectionMode= DataGridViewSelectionMode.FullRowSelect;
+            FillData();
+            this.button1.Click+=new EventHandler(this.button1_Click);
+        }
+        void FillData()
+        {
+            
+            conn = new SqlConnection(getConnectionString());
+            ds = new DataSet();
+           
+           
+            daShops = new SqlDataAdapter(getParentQuery(), conn);
+            SqlCommandBuilder cb = new SqlCommandBuilder(daShops);
+            daShops.Fill(ds, getParentTable());
+
+            daCoffeeTables = new SqlDataAdapter(getChildQuery(), conn);
+            cb = new SqlCommandBuilder(daCoffeeTables);
+            daCoffeeTables.Fill(ds, getChildTable());
+
+            DataRelation drel = new DataRelation("fk_child_parent",
+                ds.Tables[getParentTable()].Columns[getPKName()],
+                ds.Tables[getChildTable()].Columns[getFKName()]);
+            ds.Relations.Add(drel);
+
+
+           
+            bsShops = new BindingSource();
+            bsShops.DataSource = ds;
+            bsShops.DataMember = getParentTable();
+
+            bsCoffeeTables = new BindingSource();
+            bsCoffeeTables.DataSource = bsShops;
+            bsCoffeeTables.DataMember = "fk_child_parent";
+
+            //cmdBuilder.GetUpdateCommand();
+            dataGridView1.DataSource=bsShops;
+            dataGridView2.DataSource=bsCoffeeTables;    
+
+        }
+
+        private string getPKName()
+        {
+            return ConfigurationManager.AppSettings["parent_table_pk"];
+        }
+
+        private string getFKName()
+        {
+            return ConfigurationManager.AppSettings["child_table_fk"];
+        }
+
+        private string getParentTable()
+        {
+            return ConfigurationManager.AppSettings["parent_table"];
+        }
+
+        private string getParentQuery()
+        {
+            return ConfigurationManager.AppSettings["parentQuery"];
+        }
+
+        private string getChildTable()
+        {
+            return ConfigurationManager.AppSettings["child_table"];
+        }
+        private string getChildQuery()
+        {
+            return ConfigurationManager.AppSettings["childQuery"];
+        }
+
+        private void button1_Click(object sender,EventArgs e)
+        {
+
+            this.daShops.Update(this.ds, getParentTable());
+            this.daCoffeeTables.Update(this.ds, getChildTable());
+
+        }
+
+        string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString.ToString();
+        }
+      
+    }
+}
